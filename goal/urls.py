c48df4161a3456ce03e0cbe0b07cdbf69bdc4dff
LAPTOP-NAHNAH\USER <nahnahlee030324@gmail.com>
2025-02-08 17:40:18 +0900

    12차(final)

--- goal/urls.py
+++ goal/urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import GoalViewSet, GoalProgressView, MonthlyReadingProgressView
 
 router = DefaultRouter()
-router.register('goal', GoalViewSet, basename='goal')
+router.register('', GoalViewSet, basename='goal')  # ✅ '/api/goal/'로 등록
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('goal/', include(router.urls)),  # ✅ `/api/goal/`에서 GoalViewSet 사용 가능
     path('progress/', GoalProgressView.as_view(), name='goal_progress'),  # ✅ 목표 진행률 조회 (그래프용)
     path('monthly-progress/', MonthlyReadingProgressView.as_view(), name='monthly_goal_progress'),  # ✅ 월별 독서량 조회 (그래프용)
 ]
